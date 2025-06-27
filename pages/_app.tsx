2a316610666500dbb4b0f8b583bd27035371b54a
ma1orek <idzikbartosz@gmail.com>
2025-06-27 13:53:11 +0200

    Force push: wymuszenie deployu i nadpisanie starego designu nowym (mostudio-master styles)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import type { AppProps } from 'next/app'
+import '../public/css/bootstrap.min.css';
+import '../public/css/animate.css';
+import '../public/css/flaticon.css';
+import '../public/css/magnific-popup.css';
+import '../public/css/owl.carousel.min.css';
+import '../public/css/owl.theme.default.min.css';
+import '../public/css/style.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
