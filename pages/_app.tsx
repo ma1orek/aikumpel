19675fa9b9d9df6961358ea597377dcda911c8ef
ma1orek <idzikbartosz@gmail.com>
2025-06-27 14:09:18 +0200

    Zakomentowanie importw Owl Carousel, fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,8 @@ import '../public/css/bootstrap.min.css';
 import '../public/css/animate.css';
 import '../public/css/flaticon.css';
 import '../public/css/magnific-popup.css';
-import '../public/css/owl.carousel.min.css';
-import '../public/css/owl.theme.default.min.css';
+// import '../public/css/owl.carousel.min.css';
+// import '../public/css/owl.theme.default.min.css';
 import '../public/css/style.css';
 
 export default function App({ Component, pageProps }: AppProps) {
