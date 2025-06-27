2f05064123388b364400c3b2b0caa15706cd5ebe
ma1orek <idzikbartosz@gmail.com>
2025-06-27 17:33:47 +0200

    fix: remove bootstrap and flaticon imports (build error fix)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import type { AppProps } from 'next/app'
-import '../public/css/bootstrap.min.css';
+// import '../public/css/bootstrap.min.css';
 import '../public/css/animate.css';
-import '../public/css/flaticon.css';
+// import '../public/css/flaticon.css';
 import '../public/css/magnific-popup.css';
 // import '../public/css/owl.carousel.min.css';
 // import '../public/css/owl.theme.default.min.css';
